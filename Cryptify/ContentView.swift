7f6db51a03e41b93842003c3472d18379f910218
Jan Babák <babakhonza@gmail.com>
2022-11-19 21:07:53 +0100

    Add: Initial commit
    generated code

---
+++ Cryptify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cryptify
+//
+//  Created by Jan Babák on 19.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cryptify/CryptifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptifyApp.swift
+//  Cryptify
+//
+//  Created by Jan Babák on 19.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
