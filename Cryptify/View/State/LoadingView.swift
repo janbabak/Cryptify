2671d29e0b430a949267fe2596aba3cc825377e9
Jan Babák <babakhonza@gmail.com>
2022-12-20 11:37:24 +0100

    Change: enhance loading look
    - make both line endings rounded
    - scale it down

--- Cryptify/View/State/LoadingView.swift
+++ Cryptify/View/State/LoadingView.swift
@@ -9,20 +9,20 @@ import SwiftUI
 
 //custom loading, source https://www.youtube.com/watch?v=wA93Dgp-G-4
 struct LoadingView: View {
-    var dimension: CGFloat = 36
+    var dimension: CGFloat = 32
     
     @State var animate = false
     
     var body: some View {
         VStack(alignment: .center) {
             Circle()
-                .trim(from: 0, to: 0.8)
+                .trim(from: 0.1, to: 0.9)
                 .stroke(
                     AngularGradient(
-                        gradient: .init(colors: [.theme.accent, .theme.accent.opacity(0)]),
+                        gradient: .init(colors: [.theme.accent.opacity(0), .theme.accent]),
                         center: .center
                     ),
-                    style: StrokeStyle(lineWidth: 7, lineCap: .round)
+                    style: StrokeStyle(lineWidth: 6, lineCap: .round)
                 )
                 .frame(width: dimension, height: dimension)
                 .rotationEffect(Angle(degrees: animate ? 360 : 0))
